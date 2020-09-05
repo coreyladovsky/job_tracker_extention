a3edc801406f576e336e4732e71ec47008f76838
Corey Ladovsky <coreyskymusic@gmail.com>
2020-09-05 14:22:00 -0400

    getting all info boxes

--- linkedInSearch.js
+++ linkedInSearch.js
@@ -1,3 +1,8 @@
 let jobTitle = document.querySelector(".jobs-details-top-card__job-title").innerText;
 let url = document.location.href
+let company = document.querySelector(
+  ".jobs-details-top-card__company-url"
+).innerText;
 
+let map_location = document.querySelector(".jobs-details-top-card__exact-location").innerText;
+let description = document.querySelector("span[data-test-job-description-text]")
